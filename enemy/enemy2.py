d0ec8b462d2cd9741b3d1b0cc8dd83bb839ddcf6
Zac Adams <zachary.adams@student.ufv.ca>
2025-11-13 20:40:57 -0500

    Small Changes + Merging

---
+++ enemy/enemy2.py
@@ -0,0 +1,79 @@
+import pygame
+from pygame.math import Vector2
+import random
+from collections import deque as qu
+from . import greedSearchFunc
+
+
+class Enemy2(pygame.sprite.Sprite):
+    def __init__(self, image, game_map, start_tile):
+        pygame.sprite.Sprite.__init__(self)
+        self.map = game_map
+        self.start_tile = start_tile
+        self.tile = self.start_tile
+        self.pos = Vector2(self.map.tile_to_pix_center(self.tile))
+        self.prev_tile = None
+        self.next_tile = None
+        self.next_pos = None
+        self.speed = 10
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.center = self.pos
+
+    def update(self):
+        self.move()
+
+    def BFS(self):
+        greedM = self.map.grid.copy()
+        startpos = (9, 18)
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        greed = greedSearchFunc.greedSearchFunc(startpos)
+        queue = qu()
+        queue.append(greed)
+        while queue:
+            aroundNode = queue.pop()
+            for pos0,pos1 in directions:
+
+                if (aroundNode.starting[0] < greedM.shape[0]-1 and aroundNode.starting[1] < greedM.shape[1]-1 and
+                        greedM[aroundNode.starting[0]+pos0, aroundNode.starting[1]+pos1] == 0):
+                    Current = greedSearchFunc.greedSearchFunc((aroundNode.starting[0]+pos0, aroundNode.starting[1]+pos1))
+                    Current.addDistane(aroundNode.distance + 1)
+                    greedM[aroundNode.starting[0]+pos0, aroundNode.starting[1]+pos1] = Current.distance
+                    queue.appendleft(Current)
+        return greedM
+
+    def move(self):
+        global next_dr, next_dc
+        mapCopy = self.BFS()
+        row, col = self.tile
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        found_move = False
+        currentLow = mapCopy[self.tile[0], self.tile[1]]
+
+        for dr,dc in directions:
+            if(row+dr < mapCopy.shape[0]-1 and col+dc < mapCopy.shape[1]-1 and currentLow > mapCopy[row+dr,col+dc] and mapCopy[row+dr,col+dc] > -1):
+                currentLow = mapCopy[row+dr,col+dc]
+                found_move = True
+                self.next_tile = (row+dr,col+dc)
+
+        if found_move:
+            row, col = self.next_tile
+            self.next_pos = Vector2(self.map.tile_to_pix_center(self.next_tile))
+            direction = self.next_pos - self.pos
+            dist = direction.length()
+        else:
+            self.kill()
+            print(f'Reached goal!: {self.tile}')
+            return
+
+        if dist > self.speed:
+            self.pos += direction.normalize() * self.speed
+
+        else:
+            self.pos = self.next_pos
+            self.prev_tile = self.tile
+            self.tile = self.next_tile
+            self.next_tile = None
+
+        self.rect.center = self.pos
+        found_move = False
